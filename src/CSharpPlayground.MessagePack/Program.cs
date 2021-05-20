3ae17b2c4f5d694aa2bb898231cb492a9fc1a157
Slim AYACHE <slim@antvoice.com>
2021-05-20 10:09:22 +0200

    Initial commit

---
+++ src/CSharpPlayground.MessagePack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPlayground.MessagePack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
